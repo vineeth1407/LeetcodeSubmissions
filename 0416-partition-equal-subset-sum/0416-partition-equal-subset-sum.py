abd72c6a0f699b0aca0b97f0792914464dcfa5b6
shiva1407 <31319750+shiva1407@users.noreply.github.com>
2024-02-20 17:25:20 +0530

    Time: 890 ms (43.72%), Space: 16.8 MB (90.80%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -2,7 +2,7 @@ import sys
 sys.setrecursionlimit(10**6)
  
 class Solution:
-    def recursion(self,nums,index,target):
+    def Recursion(self,nums,index,target):
         if target==0:
             return True
         
@@ -19,16 +19,19 @@ class Solution:
     
     
     
-    def dpapproach(self,nums,target):
+    def DpApproach(self,nums,target):
         n = len(nums)
         dp = [[False for j in range(target+1)] for i in range(n+1)]
         
+        # if target is zero, it is true for every index
         for i in range(n+1):
             dp[i][0]=True
         
+        # if index is 0 and we still have target , it cannot be achieved
         for i in range(1,target+1):
             dp[0][i]= False
         
+        #bottom-up approach
         for i in range(1,n+1):
             for j in range(1,target+1):
                 if j<nums[i-1]:
@@ -39,12 +42,34 @@ class Solution:
         return dp[n][target]
     
     
+    def DPSpaceOptimized(self,nums,target):
+        n = len(nums)
+        dp = [False] * (target+1)
+        
+        #if target is zero, it is true
+        dp[0]= True
+        
+        for i in range(1,n+1):
+            cur = [False]*(target+1)
+            for j in range(1,target+1):
+                if j<nums[i-1]:
+                    cur[j] = dp[j]
+                else:
+                    cur[j] = dp[j] or dp[j-nums[i-1]]
+            dp = cur
+    
+        
+        return dp[target]
+        
     def canPartition(self, nums: List[int]) -> bool:
         total_sum = sum(nums)
         if total_sum%2!=0:
             return False
         required_sum = total_sum//2
         
-        #return self.recursion(nums,len(nums)-1,required_sum)
-        return self.dpapproach(nums,required_sum)
+        #return self.Recursion(nums,len(nums)-1,required_sum)
+        
+        #return self.DpApproach(nums,required_sum)
+        
+        return self.DPSpaceOptimized(nums,required_sum)
         
