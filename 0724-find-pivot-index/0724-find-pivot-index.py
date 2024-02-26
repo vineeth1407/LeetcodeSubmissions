6ba564378382d3e4950bb1326945d02ed09680ea
shiva1407 <31319750+shiva1407@users.noreply.github.com>
2024-02-26 18:25:20 +0530

    Time: 115 ms (91.36%), Space: 17.7 MB (97.30%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,4 +1,21 @@
 class Solution:
+    def approach1(self,nums):
+        left_sum = [0] * len(nums)
+        right_sum = [0]* len(nums)
+        
+        for i in range(1,len(nums)):
+            left_sum[i] = left_sum[i-1] +nums[i-1]
+        
+        for i in range(len(nums)-2,-1,-1):
+            right_sum[i] = right_sum[i+1] +nums[i+1]
+        
+        # print(left_sum)
+        # print(right_sum)
+        for i in range(len(nums)):
+            if left_sum[i]==right_sum[i]:
+                return i
+        return -1
+    
     def pivotIndex(self, nums: List[int]) -> int:
           total_sum = sum(nums)
           left_sum = 0
