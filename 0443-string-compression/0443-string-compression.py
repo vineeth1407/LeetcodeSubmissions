607bd483b1f758db23bf93f81f6772b713185356
shiva1407 <31319750+shiva1407@users.noreply.github.com>
2024-02-26 11:19:21 +0530

    Time: 58 ms (64.81%), Space: 16.6 MB (99.19%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,31 @@
+class Solution:
+    def add_count(self,count):
+        c = str(count)
+        return [i for i in c]
+       
+    def compress(self, chars: List[str]) -> int:
+        i = 0
+        res = 0
+        
+        while i < len(chars):
+            grp_len = 1
+            while (i+grp_len) < len(chars) and chars[i+grp_len]==chars[i]:
+                 grp_len+=1
+            
+            chars[res] = chars[i]
+            res+=1
+            if grp_len > 1:
+                str_rep = str(grp_len)
+                chars[res:res+len(str(str_rep))] = list(str_rep)
+                res+= len(str_rep)
+        
+            i+=grp_len
+        
+        return res
+        
+                
+                    
+                    
+                
+                
+        
