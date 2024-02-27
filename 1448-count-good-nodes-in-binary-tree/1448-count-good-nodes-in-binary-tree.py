6df05b7dcd02454200c53fe4fbb7d9276debc54a
shiva1407 <31319750+shiva1407@users.noreply.github.com>
2024-02-27 17:49:47 +0530

    Time: 147 ms (29.65%), Space: 31.3 MB (66.49%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,33 +6,48 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        queue = []
-        pathtoleaf = []
         
-        queue.append((root,[]))
+        def dfs(node, max_val):
+            if not node:
+                return 0
+            # Count the current node if it is a "good" node
+            count = 1 if node.val >= max_val else 0
+            # Update the maximum value encountered so far
+            max_val = max(max_val, node.val)
+            # Recursively visit left and right subtrees
+            count += dfs(node.left, max_val)
+            count += dfs(node.right, max_val)
+            return count
+
+    # Start DFS traversal from the root with initial max value as negative infinity
+        return dfs(root, float('-inf'))
+#         queue = []
+#         pathtoleaf = []
         
-        while queue:
-            node,path=queue.pop(0)
-            if node.left:
-                queue.append((node.left,path+[node]))
-            if node.right:
-                queue.append((node.right,path+[node]))
-            if not node.left and not node.right:
-                pathtoleaf.append((path+[node]))
+#         queue.append((root,[]))
+        
+#         while queue:
+#             node,path=queue.pop(0)
+#             if node.left:
+#                 queue.append((node.left,path+[node]))
+#             if node.right:
+#                 queue.append((node.right,path+[node]))
+#             if not node.left and not node.right:
+#                 pathtoleaf.append((path+[node]))
         
         
         
-        res=set()
-        for path in pathtoleaf:
-            for j in range(len(path)-1,-1,-1):
-                flag=True
-                for i in range(j-1,-1,-1):
-                    if path[j].val<path[i].val:
-                        flag=False
-                        break
-                if flag:
-                    res.add(path[j])
+#         res=set()
+#         for path in pathtoleaf:
+#             for j in range(len(path)-1,-1,-1):
+#                 flag=True
+#                 for i in range(j-1,-1,-1):
+#                     if path[j].val<path[i].val:
+#                         flag=False
+#                         break
+#                 if flag:
+#                     res.add(path[j])
            
-        return len(res)
+#         return len(res)
                         
         
