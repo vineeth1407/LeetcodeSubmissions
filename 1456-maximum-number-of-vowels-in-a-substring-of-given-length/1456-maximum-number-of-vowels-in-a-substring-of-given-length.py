044253f5f85bce3732eb9b8d162a53828ee1bbe2
shiva1407 <31319750+shiva1407@users.noreply.github.com>
2024-03-25 09:13:08 +0530

    Time: 126 ms (54.01%), Space: 17.2 MB (90.82%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,28 +1,29 @@
 class Solution:
-    def is_vowel(self,c):
-        return c in ['a','e','i','o','u']
+    
+    def get_count(self,vowel_dict):
+        c =0
+        for key,value in vowel_dict.items():
+            c+=value
+        return c
+    
     
     def maxVowels(self, s: str, k: int) -> int:
-        vowel_count = None
-        max_count = 0
-
-        for i in range(0, len(s) - k + 1):
-            if vowel_count is None:
-                g = 0
-                for j in range(i, i + k):
-                    if self.is_vowel(s[j]):
-                        g += 1
-                vowel_count = g
-                max_count = vowel_count
-            else:
-                if self.is_vowel(s[i - 1]):
-                    vowel_count -= 1
-                if self.is_vowel(s[i + k - 1]):
-                    vowel_count += 1
-                max_count = max(max_count, vowel_count)
-        return max_count
-
-                    
-                    
+        res = 0
+        vowel_dict= {"a":0,"e":0,"i":0,"o":0,"u":0}
+        for i in range(k):
+            if s[i] in vowel_dict:
+                vowel_dict[s[i]]+=1
+        
+        res = max(res,self.get_count(vowel_dict))
+        
+        for i in range(k,len(s)):
+            if s[i-k] in vowel_dict:
+                vowel_dict[s[i-k]]-=1
+            if s[i] in vowel_dict:
+                vowel_dict[s[i]]+=1
+                res = max(res,self.get_count(vowel_dict))
+        return res
+            
+            
             
         
